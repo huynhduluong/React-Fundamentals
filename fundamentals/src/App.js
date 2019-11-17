095bebeee27e573b0f203853ed4d98353d07764e
Huynh Du Luong <huynhduluong@gmail.com>
2019-11-17 13:44:02 +0700

    props

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
+import Intro from './component/Intro/index';
 import './App.css';
-
-const Intro = (props) => (
-  <p className="App-intro">
-    Our first functional component
-  </p>
-)
 
 class App extends Component {
   render() {
@@ -14,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">TV Series Lists</h1>
         </header>
-        <Intro />
+        <Intro message="Here you can find all of your most loved series"/>
       </div>
     );
   }
