afaf97ee5f4f5ef11d4c048333cf1437feba3dee
Huynh Du Luong <huynhduluong@gmail.com>
2019-11-17 15:32:37 +0700

    Asynchronous Calls

--- fundamentals/src/component/App/index.js
+++ fundamentals/src/component/App/index.js
@@ -7,6 +7,11 @@ class App extends Component {
     series: []
   }
 
+  componentDidMount() {
+    fetch(`http://api.tvmaze.com/search/shows?q=Vikings`)
+      .then((response) => response.json())
+      .then(json => this.setState( {series: json}))
+  }
 
   render() {
     return (
